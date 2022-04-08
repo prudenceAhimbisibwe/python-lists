4e3e31a2b73583af7126192db7851ce1c6889517
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-04-08 09:17:44 +0300

    first changes

---
+++ main.py
@@ -0,0 +1,28 @@
+numbers=[3,7,9,13]
+print(numbers)
+
+random_list=[5.9,"Hello",-6,8.2]
+list1=[]
+print(list1)
+lanquages=["python","kotlin","javascipt"]
+print(lanquages)
+print(lanquages[0])
+print(lanquages[0:2])
+lanquages[1]="Ruby"
+print(lanquages)
+lanquages[2:4]="React","js","Angular"
+print(lanquages)
+print("python" in lanquages)
+print("c++" in lanquages)
+lanquages.append("Rust")
+print(lanquages)
+lanquages.insert(0,"CSS")
+print(lanquages)
+lanquages.remove("python")
+print(lanquages)
+# tuples
+numbers=(52,-6,9,4)
+print(numbers)
+for number in numbers:
+    print(number)
+
